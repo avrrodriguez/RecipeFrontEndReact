0bb85d042e8b25c7bd120eb7f573efab902f263d
Abraham V Rodriguez <pqavrr835@gmail.com>
2024-08-25 14:11:12 -0400

    adds dynamic font sizing to index css, adds a height and padding to page container in app csss, adds a login and sign up pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,20 @@ export function Header() {
   return (
     <header>
       <div className="top-bar-header">
-        <nav>
-          <a href="/">Home</a>
+        <nav className="header-menu">
+          <div className="menu-left">
+            <a className="site-button header-button" href="/">
+              Home
+            </a>
+          </div>
+          <div className="menu-right">
+            <a className="site-button header-button" href="/login">
+              Log In
+            </a>
+            <a className="site-button header-button" href="/signup">
+              Sign Up
+            </a>
+          </div>
         </nav>
       </div>
     </header>
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+export function Signup(props) {
+  return (
+    <div>
+      <h1>{props.pageTitle}</h1>
+      <p>Sign up Form</p>
+    </div>
+  );
+}
