f18b145298228e68801d8495e89942842d4a30ff
Abraham V Rodriguez <pqavrr835@gmail.com>
2025-12-04 07:56:20 -0500

    changes to import strings

--- src/helpers/SearchFormFilter.jsx
+++ src/helpers/SearchFormFilter.jsx
@@ -1,24 +1,21 @@
-import { recipeSearchFormSampleList } from "../../public/SearchFormSampleLists/RecipeSearchFormSampleList";
-import { cookingStyleFormSampleList } from "../../public/SearchFormSampleLists/CookingStyleFormSampleList";
-import { ingredientsSampleList } from "../../public/SearchFormSampleLists/IngredientsSampleList";
+import { recipeSearchFormSampleList } from "/public/SearchFormSampleLists/RecipeSearchFormSampleList";
+import { cookingStyleFormSampleList } from "/public/SearchFormSampleLists/CookingStyleFormSampleList";
+import { ingredientsSampleList } from "/public/SearchFormSampleLists/IngredientsSampleList";
 
 export function SearchFormFilter(category, searchInput) {
   var filteredList;
 
   switch (category) {
     case "Recipe Name":
-      filteredList = recipeSearchFormSampleList
-        .filter((sample) => sample.name.includes(searchInput))
+      filteredList = recipeSearchFormSampleList.filter((sample) => sample.name.includes(searchInput));
       break;
     case "Ingredients":
-      filteredList = ingredientsSampleList
-        .filter((sample) => sample.name.includes(searchInput))
+      filteredList = ingredientsSampleList.filter((sample) => sample.name.includes(searchInput));
       break;
     case "Cooking Style":
-      filteredList = cookingStyleFormSampleList
-        .filter((sample) => sample.name.includes(searchInput))
+      filteredList = cookingStyleFormSampleList.filter((sample) => sample.name.includes(searchInput));
       break;
   }
-  
+
   return filteredList;
 }
